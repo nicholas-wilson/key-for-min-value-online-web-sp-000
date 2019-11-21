aa1428505b19749ddabe3b3821ee4f251e5b2130
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-21 00:21:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_value = 0
+  key = "bob"
+  current_iteration = 0
+  name_hash.each do |name, value|
+    if current_iteration == 0
+      lowest_value = value
+      key = name
+    end
+    if lowest_value > value
+      lowest_value = value
+      key = name
+    end
+    current_iteration += 1
 end
